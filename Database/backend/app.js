19290414fbde5927d46cb160788cfe8a9b5df0d5
Chaitanya Vasu <chaitanya.vasu@nagarro.com>
2023-12-11 11:30:24 +0530

    files uploaded

---
+++ Database/backend/app.js
@@ -0,0 +1,27 @@
+const express = require ('express');
+const bodyparser =require('body-parser');
+const cors=require('cors');
+
+
+
+const app=express()
+const port=4300
+
+app.use(bodyparser.json());
+app.use(cors());
+
+//database connection
+// app.get('/', (req, res) => {
+//     res.send('Hello, this is the root path!');
+//   });
+  
+
+//listen on port 4300
+app.listen(port,()=>console.log(`Listening on port ${port}`))
+
+
+//Routes
+const routeRouter= require('./routes/route')
+
+
+app.use('/',routeRouter)
